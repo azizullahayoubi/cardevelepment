c8aea8d04a0fb69d7092da7a3767298b03c10084
azizullahayoubi <azizullahayoubi@gmail.com>
2021-06-12 11:14:51 -0400

    basic feature of the car added and car object created

---
+++ src/com/cardevelopment/Car.java
@@ -0,0 +1,11 @@
+package com.cardevelopment;
+
+public class Car {
+    public static void main(String[] args) {
+        String color;
+        String Model;
+        String number;
+        String door;
+
+    }
+}
---
+++ src/com/cardevelopment/Main.java
@@ -0,0 +1,10 @@
+package com.cardevelopment;
+
+import java.util.concurrent.Callable;
+
+public class Main {
+    public static void main(String[] args) {
+        Car audi=new Car();
+        
+    }
+}
